95526a249d8ae7aeb0b911bfa480807f0e9efb2b
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-13 15:30:57 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import {
   createStackNavigator,
@@ -8,26 +8,36 @@ import {
 import { StatusBar } from "expo-status-bar";
 import Home from "./screens/Home";
 import FirstImageCarousel from "./screens/FirstImageCarousel/FirstImageCarousel";
-import OnBoarding from "./screens/OnBoarding/OnBoarding";
+import Tabs from "./screens/BottomTabs/Tabs";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
-          ...TransitionPresets.ModalSlideFromBottomIOS,
           headerShown: false,
+          ...TransitionPresets.ModalSlideFromBottomIOS,
         }}
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Parallax Carousel" component={FirstImageCarousel} />
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
+        <Stack.Screen name="Tabs" component={Tabs} />
       </Stack.Navigator>
-
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
+
+export default App;
