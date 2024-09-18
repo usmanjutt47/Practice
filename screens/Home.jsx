743bcad6d852f01341ca57ecb8e5b15e8cda3768
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-18 15:11:04 +0500

    Update Home.jsx

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,4 +1,10 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -6,49 +12,72 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   const navigation = useNavigation();
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#f4f6f6" }}>
-      <ScrollView
-        contentContainerStyle={{
-          flex: 1,
-          backgroundColor: "#f4f6f6",
-          alignItems: "center",
-          flexGrow: 1,
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#f4f6f6", alignItems: "center" }}
+    >
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Parallax Carousel")}
+        style={{
+          height: 48,
+          width: "90%",
+          backgroundColor: "#006816",
+          borderRadius: 10,
+          justifyContent: "center",
+          marginTop: "5%",
         }}
       >
-        <Pressable
-          onPress={() => navigation.navigate("Parallax Carousel")}
-          style={{
-            height: 48,
-            width: "90%",
-            backgroundColor: "#212f3c",
-            borderRadius: 10,
-            justifyContent: "center",
-            marginTop: "5%",
-          }}
-        >
-          <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-            Parallax Carousel
-          </Text>
-        </Pressable>
+        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
+          Parallax Carousel
+        </Text>
+      </TouchableOpacity>
 
-        <Pressable
-          onPress={() => navigation.navigate("Tabs")}
-          style={{
-            height: 48,
-            width: "90%",
-            backgroundColor: "#212f3c",
-            borderRadius: 10,
-            justifyContent: "center",
-            marginTop: "5%",
-          }}
-        >
-          <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-            Custom Bottom Tab
-          </Text>
-        </Pressable>
-      </ScrollView>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Tabs")}
+        style={{
+          height: 48,
+          width: "90%",
+          backgroundColor: "#003468",
+          borderRadius: 10,
+          justifyContent: "center",
+          marginTop: "5%",
+        }}
+      >
+        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
+          Custom Bottom Tab
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("CustomDrawer")}
+        style={{
+          height: 48,
+          width: "90%",
+          backgroundColor: "#748dea",
+          borderRadius: 10,
+          justifyContent: "center",
+          marginTop: "5%",
+        }}
+      >
+        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
+          Drawer
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("CustomAnimatedDrawer")}
+        style={{
+          height: 48,
+          width: "90%",
+          backgroundColor: "#dd8bf3",
+          borderRadius: 10,
+          justifyContent: "center",
+          marginTop: "5%",
+        }}
+      >
+        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
+          Drawer
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({});
