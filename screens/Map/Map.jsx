c9ad6bf26de40e9a60ee3ec966bebd1b0acaadf4
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-19 10:37:57 +0500

    Update Map.jsx

--- screens/Map/Map.jsx
+++ screens/Map/Map.jsx
@@ -2,20 +2,25 @@ import React, { useState, useEffect, useRef } from "react";
 import {
   StyleSheet,
   Alert,
-  TouchableOpacity,
+  Pressable,
   TextInput,
   View,
   Keyboard,
+  Text,
+  ActivityIndicator,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { Marker } from "react-native-maps";
 import { MaterialIcons } from "@expo/vector-icons";
 import * as Location from "expo-location";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function Map() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [search, setSearch] = useState("");
+  const [mapType, setMapType] = useState("standard");
+  const [loading, setLoading] = useState(false);
   const mapRef = useRef(null);
 
   useEffect(() => {
@@ -70,9 +75,11 @@ export default function Map() {
 
   const handleSearchSubmit = async () => {
     try {
+      setLoading(true);
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         Alert.alert("Permission denied", "Unable to access location");
+        setLoading(false);
         return;
       }
 
@@ -104,12 +111,22 @@ export default function Map() {
     } catch (error) {
       Alert.alert("Error", "An error occurred while searching");
       console.error("Search Error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleClearSearch = () => {
     setSearch("");
-    Keyboard.dismiss(); // Dismiss keyboard on clear
+    Keyboard.dismiss();
+  };
+
+  const toggleTo3D = () => {
+    setMapType("hybrid");
+  };
+
+  const toggleToNormal = () => {
+    setMapType("standard");
   };
 
   return (
@@ -120,21 +137,19 @@ export default function Map() {
           placeholder="Search for a location"
           value={search}
           onChangeText={setSearch}
-          returnKeyType="search" // Show search key on keyboard for single word
-          onSubmitEditing={handleSearchSubmit} // Trigger search on submit
+          returnKeyType="search"
+          onSubmitEditing={handleSearchSubmit}
         />
         {search.length > 0 && (
-          <TouchableOpacity
-            style={styles.clearButton}
-            onPress={handleClearSearch}
-          >
+          <Pressable style={styles.clearButton} onPress={handleClearSearch}>
             <MaterialIcons name="cancel" size={24} color="black" />
-          </TouchableOpacity>
+          </Pressable>
         )}
       </View>
       <MapView
         style={styles.map}
         ref={mapRef}
+        mapType={mapType}
         initialRegion={{
           latitude: location ? location.latitude : 37.78825,
           longitude: location ? location.longitude : -122.4324,
@@ -142,6 +157,8 @@ export default function Map() {
           longitudeDelta: 0.0421,
         }}
         showsUserLocation={true}
+        showsCompass={false}
+        showsMyLocationButton={false}
       >
         {location && (
           <Marker
@@ -152,12 +169,28 @@ export default function Map() {
           />
         )}
       </MapView>
-      <TouchableOpacity
+
+      {loading && (
+        <ActivityIndicator size="large" color="black" style={styles.loader} />
+      )}
+
+      <Pressable
         style={styles.locationButton}
         onPress={handleCurrentLocationPress}
       >
-        <MaterialIcons name="my-location" size={40} color="black" />
-      </TouchableOpacity>
+        <MaterialIcons name="my-location" size={30} color="black" />
+      </Pressable>
+
+      <Pressable style={styles.threeDButton} onPress={toggleTo3D}>
+        <MaterialIcons name="3d-rotation" size={30} color="black" />
+      </Pressable>
+
+      <Pressable style={styles.normalButton} onPress={toggleToNormal}>
+        <FontAwesome5 name="route" size={30} color="black" />
+      </Pressable>
+      <Pressable style={styles.compass} onPress={toggleToNormal}>
+        <FontAwesome5 name="compass" size={30} color="black" />
+      </Pressable>
     </SafeAreaView>
   );
 }
@@ -175,12 +208,15 @@ const styles = StyleSheet.create({
     right: 10,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 10,
+    height: 60,
+    width: 60,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
   },
   searchContainer: {
     position: "absolute",
@@ -203,14 +239,66 @@ const styles = StyleSheet.create({
     height: 40,
     paddingHorizontal: 10,
   },
-  searchButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginRight: 10,
-  },
   clearButton: {
     justifyContent: "center",
     alignItems: "center",
     marginRight: 10,
+  },
+  threeDButton: {
+    position: "absolute",
+    bottom: 80,
+    right: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 60,
+    width: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  normalButton: {
+    position: "absolute",
+    bottom: 150,
+    right: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 60,
+    width: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  compass: {
+    position: "absolute",
+    bottom: 220,
+    right: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 60,
+    width: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loader: {
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
 });
