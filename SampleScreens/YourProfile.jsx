0c1831c8e3ba32bc97f2aeda691ff16015277dc9
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-20 12:46:35 +0500

    Update YourProfile.jsx

--- SampleScreens/YourProfile.jsx
+++ SampleScreens/YourProfile.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function YourProfile() {
+export default function YourProfile({ drawerStyles }) {
   return (
-    <View>
+    <View
+      style={{
+        ...drawerStyles,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "#f3f3f3",
+      }}
+    >
       <Text>YourProfile</Text>
     </View>
-  )
+  );
 }
