965104b9df66be2199837fb5844698f8c76ee773
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-20 12:44:13 +0500

    Create LikedStories.jsx

---
+++ SampleScreens/LikedStories.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function LikedStories() {
+  return (
+    <View>
+      <Text>LikedStories</Text>
+    </View>
+  )
+}
