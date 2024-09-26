026288306fd770bbdce8e6f6caf73859b1017365
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-09-26 12:49:10 +0500

    Update Home.jsx

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -12,148 +12,108 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   const navigation = useNavigation();
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: "#f4f6f6", alignItems: "center" }}
-    >
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Parallax Carousel")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#006816",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#ffff" }}>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          alignItems: "center",
+          paddingVertical: 20,
         }}
       >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Parallax Carousel
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Parallax Carousel")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Parallax Carousel</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Tabs")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#003468",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Custom Bottom Tab
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Tabs")}
+          style={[styles.button, { backgroundColor: "#003468" }]}
+        >
+          <Text style={styles.buttonText}>Custom Bottom Tab</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("CustomDrawer")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#748dea",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Drawer
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("CustomDrawer")}
+          style={[styles.button, { backgroundColor: "#748dea" }]}
+        >
+          <Text style={styles.buttonText}>Drawer</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("CustomAnimatedDrawer")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#dd8bf3",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Custom Drawer
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Map")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#a86732",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Map
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("ImageHome")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#428df5",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Custom Image Crousel
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("InputHome")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#f0a535",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Text Input
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("CustomAnimatedDrawer")}
+          style={[styles.button, { backgroundColor: "#dd8bf3" }]}
+        >
+          <Text style={styles.buttonText}>Custom Drawer</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("OrderStatusScreen")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#5332a8",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Vertical Order Status
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("AnimatedDrawer")}
-        style={{
-          height: 48,
-          width: "90%",
-          backgroundColor: "#d1f542",
-          borderRadius: 10,
-          justifyContent: "center",
-          marginTop: "5%",
-        }}
-      >
-        <Text style={{ color: "#fff", textAlign: "center", fontSize: 18 }}>
-          Vertical Order Status
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Map")}
+          style={[styles.button, { backgroundColor: "#a86732" }]}
+        >
+          <Text style={styles.buttonText}>Map</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ImageHome")}
+          style={[styles.button, { backgroundColor: "#428df5" }]}
+        >
+          <Text style={styles.buttonText}>Custom Image Crousel</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("InputHome")}
+          style={[styles.button, { backgroundColor: "#f0a535" }]}
+        >
+          <Text style={styles.buttonText}>Text Input</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("OrderStatusScreen")}
+          style={[styles.button, { backgroundColor: "#5332a8" }]}
+        >
+          <Text style={styles.buttonText}>Vertical Order Status</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("AnimatedDrawer")}
+          style={[styles.button, { backgroundColor: "#d1f542" }]}
+        >
+          <Text style={styles.buttonText}>Vertical Order Status</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("CustomButtons")}
+          style={[styles.button, { backgroundColor: "#868686" }]}
+        >
+          <Text style={styles.buttonText}>Custom Buttons</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ButtonApp")}
+          style={[styles.button, { backgroundColor: "#5332d2" }]}
+        >
+          <Text style={styles.buttonText}>Button</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    height: 48,
+    width: "90%",
+    backgroundColor: "#006816",
+    borderRadius: 10,
+    justifyContent: "center",
+    marginTop: "5%",
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
